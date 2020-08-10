1ff19c3a595c2ad2c3be568756c118a3678ce61a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-08-10 19:01:29 +0530

    Created Simple Login

--- main/urls.py
+++ main/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
-    path('admin/', admin.site.urls),
-    
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),    
 ]
